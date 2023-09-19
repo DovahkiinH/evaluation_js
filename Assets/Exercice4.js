e045a4410a49e2c419d3db10da564aea74c93a96
DovahkiinH <loick.caplier@gmail.com>
2023-09-19 15:19:55 +0200

    Evaluation

---
+++ Assets/Exercice4.js
@@ -0,0 +1,37 @@
+function Commande () {
+
+    var PU = window.prompt("Prix Unitaire de ce produit :");
+    var QTECOM = window.prompt("La Quantité que vous souhaitez acheter :");
+    var TOT = (PU * QTECOM);
+
+    if ((TOT => 100) && (TOT <= 200)) {
+
+        var remise = TOT * 0.05 ;
+        var PRIXREM = TOT - remise ;
+    } else if (TOT> 200)
+
+    {
+
+        var remise = TOT * 0.1;
+        var PRIXREM = TOT - remise ;
+
+    }
+
+    if (PRIXREM => 500) {
+
+        var FPORT = 0;
+
+    }else 
+    
+    {
+
+        var FPORT = Math.max(PRIXREM * 0.02 , 6);
+
+    }
+
+    var PRIXFINAL = +PRIXREM + +FPORT;
+
+    alert("La remise est de " + remise + " € soit " + PRIXREM + " € et frais de port de " + FPORT + " € le prix total est de " + PRIXFINAL + "€");
+}
+
+Commande();
